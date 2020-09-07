16e95982016850e23499d04e460f71d394c47c37
Greeshma Sadineni <greeshma.sadineni@gmail.com>
2020-09-07 11:05:23 +0200

    Employee Json file is created

---
+++ JSON Object Creation/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Object_Creation
+{
+    class Employee
+    {
+        public string FirstName = "Greeshma";
+        public string lastName = "Sadineni";
+        public int EmployeeId = 5001;
+        public string Designation = "Manager";
+       
+
+
+    }
+}
