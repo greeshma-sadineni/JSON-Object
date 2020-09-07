3f3dc40e0db65138f631a16f4d97b39452c2f7c5
Greeshma Sadineni <greeshma.sadineni@gmail.com>
2020-09-07 13:22:45 +0200

    Student Json Completed

---
+++ StudentJSON/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace StudentJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+            //List<Student> stu = new List<Student>()
+            //{
+            //   new Student
+            //   {
+            //       Name="Raji",
+            //       Class=4,
+            //       RollNo=1,
+            //       Subject=new string[]
+            //       {
+            //           "Java",
+            //           "C"
+
+            //       }
+            //   },
+            //   new Student
+            //    {
+            //       Name="Vinay",
+            //       Class=4,
+            //       RollNo=1,
+            //       Subject=new string[]
+            //       {
+            //           "Asp",
+            //           "Rpa"
+
+            //       }
+            //   }
+            //};
+    string JSONresult = JsonConvert.SerializeObject(stu);
+            string path = @"F:\API\JSON\Student.Json";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+            else if (!File.Exists(path))
+            {
+                using (var tw = new StreamWriter(path, true))
+                {
+                    tw.WriteLine(JSONresult.ToString());
+                    tw.Close();
+                }
+            }
+
+        }
+    }
+}
---
+++ StudentJSON/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentJSON
+{
+     public class Student
+    {
+        public string Name = "Greeshma";
+        public int Class = 4;
+        public int RollNo = 470;
+        public string[] Subject = { "Asp", "c#", "Java" };
+
+        //public string Name { set; get; }
+        //public int Class { set; get; }
+        //public int RollNo { set; get; }
+        //public string[] Subject { set; get; }
+
+
+    }
+}
